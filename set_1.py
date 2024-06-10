8acc90b45d4aecfcf3664a28dca72efda6afb652
Amanda  Prats <amandaprats@Amandas-MacBook-Air.local>
2024-06-11 02:36:37 +0800

    set 1 recommit

--- set_1.py
+++ set_1.py
@@ -67,7 +67,38 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    def material_waste(total_material, material_units, num_jobs, job_consumption):
-        used_materials = num_jobs*job_consumption
-        waste = total_material - used_materials
-        return str(waste) + material_units
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    used_materials = num_jobs*job_consumption
+    waste = total_material - used_materials
+    return str(waste) + material_units
+
+def interest(principal, rate, periods):
+    '''Interest.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+def interest(principal, rate, periods):
+    si = (principal * rate * periods) + principal
+    return int(si)
